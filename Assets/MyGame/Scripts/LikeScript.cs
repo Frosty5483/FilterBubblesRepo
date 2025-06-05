5b93c3ebbe9731939f0a1812bfbf5a5b0cf8122d
Frosty5483 <75914780+Frosty5483@users.noreply.github.com>
2025-06-05 11:42:14 +0200

    Level 2 nearly done

--- Assets/MyGame/Scripts/LikeScript.cs
+++ Assets/MyGame/Scripts/LikeScript.cs
@@ -9,18 +9,234 @@ public class LikeScript : MonoBehaviour
     [SerializeField] SpriteRenderer post2_imag;
     [SerializeField] TMP_Text post1_txt;
     [SerializeField] TMP_Text post2_txt;
-    
-    private Manager manag;
+
+    private bool tempBool;
+    private bool tempBoolS;
+    private Manager manager;
 
     private void Start()
     {
-        manag = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+        if (manager.postRoutineE == 1 && manager.postRoutineS == 1)
+        {
+            StartCoroutine(GoToNewPostE(manager.post1E_Imag, manager.post1E_Txt, manager.post1S_Imag, manager.post1S_Txt));
+            manager.postRoutineS += 1;
+        }
     }
-    void GoToNewPost(Sprite image1, string text1, Sprite image2, string text2)
+
+
+
+    public IEnumerator GoToNewPostE(Sprite image1, string text1, Sprite image2, string text2)
     {
         post1_imag.sprite = image1;
         post2_imag.sprite = image2;
         post1_txt.text = text1;
         post2_txt.text = text2;
+
+        yield return new WaitForSeconds(0.2f);
+        if (tempBool == false)
+        {
+            manager.postRoutineE += 1;
+        }
+        if (tempBool == true)
+        {
+            manager.postRoutineE = 6;
+            tempBool = false;
+        }
+
     }
+
+    public IEnumerator GoToNewPostS(Sprite image1, string text1, Sprite image2, string text2)
+    {
+        post1_imag.sprite = image1;
+        post2_imag.sprite = image2;
+        post1_txt.text = text1;
+        post2_txt.text = text2;
+
+        yield return new WaitForSeconds(0.2f);
+        if (tempBoolS == false)
+        {
+            manager.postRoutineS += 1;
+        }
+        if (tempBoolS == true)
+        {
+            manager.postRoutineS = 6;
+            tempBoolS = false;
+        }
+    }
+    private void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //E-path Left
+            if (manager.postRoutineE == 2 && gameObject.tag == "Like1" && manager.epath == false && manager.spath == false)
+            {
+                //E 1 --> E 2/3
+                manager.epath = true;
+                manager.post1E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post2E_Imag, manager.post2E_Txt, manager.post3E_Imag, manager.post3E_Txt));
+            }
+
+            if (manager.postRoutineE == 3 && gameObject.tag == "Like1" && manager.epath == true && manager.spath == false)
+            {
+                //E 2 --> E 4/5
+                manager.post2E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post4E_Imag, manager.post4E_Txt, manager.post5E_Imag, manager.post5E_Txt));
+            }
+            if (manager.postRoutineE == 4 && gameObject.tag == "Like1" && manager.epath == true && manager.spath == false)
+            {
+                //E 4 --> E 7/8
+                manager.post4E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post7E_Imag, manager.post7E_Txt, manager.post8E_Imag, manager.post8E_Txt));
+            }
+
+            if (manager.postRoutineE == 6 && gameObject.tag == "Like1" && manager.epath == true && manager.spath == false)
+            {
+                //E 5 --> E 8/9
+                manager.post5E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post8E_Imag, manager.post8E_Txt, manager.post9E_Imag, manager.post9E_Txt));
+            }
+
+
+            if (manager.postRoutineE == 7 && gameObject.tag == "Like1" && manager.epath == true && manager.spath == false)
+            {
+                //E 9 --> LoadAfter
+                manager.post9E_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+            if (manager.postRoutineE == 5 && gameObject.tag == "Like1" && manager.epath == true && manager.spath == false)
+            {
+                //E 7 --> LoadAfter
+                manager.post7E_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+
+            //E-path Right
+            if (manager.postRoutineE == 3 && gameObject.tag == "Like2" && manager.epath == true && manager.spath == false)
+            {
+                //E 3 --> E 5/6
+                tempBool = true;
+                manager.post3E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post5E_Imag, manager.post5E_Txt, manager.post6E_Imag, manager.post6E_Txt));
+            }
+
+            if (manager.postRoutineE == 4 && gameObject.tag == "Like2" && manager.epath == true && manager.spath == false)
+            {
+                //E 5 --> E 8/9
+                manager.post5E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post8E_Imag, manager.post8E_Txt, manager.post9E_Imag, manager.post9E_Txt));
+            }
+
+            if (manager.postRoutineE == 6 && gameObject.tag == "Like2" && manager.epath == true && manager.spath == false)
+            {
+                //E 6 --> E 9/10
+                manager.post6E_bool = true;
+                StartCoroutine(GoToNewPostE(manager.post9E_Imag, manager.post9E_Txt, manager.post10E_Imag, manager.post10E_Txt));
+            }
+
+            if (manager.postRoutineE == 7 && gameObject.tag == "Like2" && manager.epath == true && manager.spath == false)
+            {
+                //E 10 --> LoadAfter
+                manager.post10E_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+            if (manager.postRoutineE == 5 && gameObject.tag == "Like2" && manager.epath == true && manager.spath == false)
+            {
+                //E 8 --> LoadAfter
+                manager.post8E_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+
+
+            //S-path Left
+            if (manager.postRoutineS == 2 && gameObject.tag == "Like2" && manager.spath == false && manager.epath == false)
+            {
+                //S 1 --> S 2/3
+                manager.spath = true;
+                manager.post1S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post2S_Imag, manager.post2S_Txt, manager.post3S_Imag, manager.post3S_Txt));
+            }
+
+            if (manager.postRoutineS == 3 && gameObject.tag == "Like1" && manager.spath == true && manager.epath == false)
+            {
+                //S 2 --> S 4/5
+                manager.post2S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post4S_Imag, manager.post4S_Txt, manager.post5S_Imag, manager.post5S_Txt));
+            }
+            if (manager.postRoutineS == 4 && gameObject.tag == "Like1" && manager.spath == true && manager.epath == false)
+            {
+                //S 4 --> S 7/8
+                manager.post4S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post7S_Imag, manager.post7S_Txt, manager.post8S_Imag, manager.post8S_Txt));
+            }
+
+            if (manager.postRoutineS == 6 && gameObject.tag == "Like1" && manager.spath == true && manager.epath == false)
+            {
+                //S 5 --> S 8/9
+                manager.post5S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post8S_Imag, manager.post8S_Txt, manager.post9S_Imag, manager.post9S_Txt));
+            }
+
+            if (manager.postRoutineS == 7 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 9 --> LoadAfter
+                manager.post9S_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+            if (manager.postRoutineS == 5 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 7 --> LoadAfter
+                manager.post7S_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+
+            //S-path Right
+            if (manager.postRoutineS == 3 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 3 --> S 5/6
+                tempBoolS = true;
+                manager.post3S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post5S_Imag, manager.post5S_Txt, manager.post6S_Imag, manager.post6S_Txt));
+            }
+
+            if (manager.postRoutineS == 4 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 5 --> S 8/9
+                manager.post5S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post8S_Imag, manager.post8S_Txt, manager.post9S_Imag, manager.post9S_Txt));
+            }
+
+            if (manager.postRoutineS == 6 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 6 --> S 9/10
+                manager.post6S_bool = true;
+                StartCoroutine(GoToNewPostS(manager.post9S_Imag, manager.post9S_Txt, manager.post10S_Imag, manager.post10S_Txt));
+            }
+
+
+            if (manager.postRoutineS == 7 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 10 --> LoadAfter
+                manager.post10S_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+            if (manager.postRoutineS == 5 && gameObject.tag == "Like2" && manager.spath == true && manager.epath == false)
+            {
+                //S 8 --> LoadAfter
+                manager.post8S_bool = true;
+                manager.LoadAfterLvl2();
+            }
+
+        }
+
+    }
+
 }
--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    public bool Post1E;
-    public bool Posts1S;
-    public bool Posts2E;
-    public bool Posts2S;
-    public bool Posts3E;
-    public bool Posts3S;
-    public bool Posts4E;
-    public bool Posts4S;
-    public bool Posts5E;
-    public bool Posts5S;
-    public bool Posts6E;
-    public bool Posts6S;
-    public bool Posts7E;
-    public bool Posts7S;
-    public bool Posts8E;
-    public bool Posts8S;
-    public bool Posts9E;
-    public bool Posts9S;
-    public bool Posts10E;
-    public bool Posts10S;
+    
+    [SerializeField] GameObject postScreen;
+    [SerializeField] GameObject radicalizeScreen;
 
+    [SerializeField] TMP_Text radicalizeText;
+
+    public string radicalized;
+
+    [Header("Radicalize Images")]
+    [SerializeField] SpriteRenderer post1_imag;
+    [SerializeField] SpriteRenderer post2_imag;
+    [SerializeField] SpriteRenderer post3_imag;
+    [SerializeField] SpriteRenderer post4_imag;
+
+    [Header("Radicalize Texts")]
+    [SerializeField] TMP_Text post1_Text;
+    [SerializeField] TMP_Text post2_Text;
+    [SerializeField] TMP_Text post3_Text;
+    [SerializeField] TMP_Text post4_Text;
+
+    [Header("Level 2 Post Help Values")]
+    public int postRoutineE = 1;
+    public int postRoutineS = 1;
+
+    public bool epath;
+    public bool spath;
+
+    [Header("Level 2 Post Images")]
     public Sprite post1E_Imag;
     public Sprite post1S_Imag;
     public Sprite post2E_Imag;
@@ -44,8 +52,9 @@ public class Manager : MonoBehaviour
     public Sprite post9E_Imag;
     public Sprite post9S_Imag;
     public Sprite post10E_Imag;
-    public Sprite post10s_Imag;
+    public Sprite post10S_Imag;
 
+    [Header("Level 2 Post Texts")]
     public string post1E_Txt;
     public string post1S_Txt;
     public string post2E_Txt;
@@ -67,4 +76,164 @@ public class Manager : MonoBehaviour
     public string post10E_Txt;
     public string post10S_Txt;
 
+    [Header("Level 2 Post Bools")]
+    public bool post1E_bool;
+    public bool post2E_bool;
+    public bool post3E_bool;
+    public bool post4E_bool;
+    public bool post5E_bool;
+    public bool post6E_bool;
+    public bool post7E_bool;
+    public bool post8E_bool;
+    public bool post9E_bool;
+    public bool post10E_bool;
+    public bool post1S_bool;
+    public bool post2S_bool;
+    public bool post3S_bool;
+    public bool post4S_bool;
+    public bool post5S_bool;
+    public bool post6S_bool;
+    public bool post7S_bool;
+    public bool post8S_bool;
+    public bool post9S_bool;
+    public bool post10S_bool;
+
+    public void LoadAfterLvl2()
+    {
+        postScreen.SetActive(false);
+        radicalizeScreen.SetActive(true);
+
+        radicalizeText.text = radicalized;
+        radicalizeText.gameObject.SetActive(true);
+
+        if (post1E_bool)
+        {
+            post1_imag.sprite = post1E_Imag;
+            post1_Text.text = post1E_Txt;
+
+        }
+        if (post1S_bool)
+        {
+            post1_imag.sprite = post1S_Imag;
+            post1_Text.text = post1S_Txt;
+
+        }
+
+
+
+        if (post2E_bool)
+        {
+            post2_imag.sprite = post2E_Imag;
+            post2_Text.text = post2E_Txt;
+
+        }
+        if (post3E_bool)
+        {
+            post2_imag.sprite = post3E_Imag;
+            post2_Text.text= post3E_Txt;
+
+        }
+        if (post2S_bool)
+        {
+            post2_imag.sprite = post2S_Imag;
+            post2_Text.text = post2S_Txt;
+
+        }
+        if (post3S_bool)
+        {
+            post2_imag.sprite = post3S_Imag;
+            post2_Text.text = post3S_Txt;
+
+        }
+
+
+
+        if (post4E_bool)
+        {
+            post3_imag.sprite = post4E_Imag;
+            post3_Text.text = post4E_Txt;
+
+        }
+        if (post5E_bool)
+        {
+            post3_imag.sprite = post5E_Imag;
+            post3_Text.text = post5E_Txt;
+
+        }
+        if (post6E_bool)
+        {
+            post3_imag.sprite = post6E_Imag;
+            post3_Text.text = post6E_Txt;
+
+        }
+        if (post4S_bool)
+        {
+            post3_imag.sprite = post4S_Imag;
+            post3_Text.text = post4S_Txt;
+
+        }
+        if (post5S_bool)
+        {
+            post3_imag.sprite = post5S_Imag;
+            post3_Text.text = post5S_Txt;
+
+        }
+        if (post6S_bool)
+        {
+            post3_imag.sprite = post6S_Imag;
+            post3_Text.text = post6S_Txt;
+
+        }
+
+
+
+        if (post7E_bool)
+        {
+            post4_imag.sprite = post7E_Imag;
+            post4_Text.text = post7E_Txt;
+
+        }
+        if (post8E_bool)
+        {
+            post4_imag.sprite = post8E_Imag;
+            post4_Text.text = post8E_Txt;
+
+        }
+        if (post9E_bool)
+        {
+            post4_imag.sprite = post9E_Imag;
+            post4_Text.text = post9E_Txt;
+
+        }
+        if (post10E_bool)
+        {
+            post4_imag.sprite = post10E_Imag;
+            post4_Text.text = post10E_Txt;
+
+        }
+        if (post7S_bool)
+        {
+            post4_imag.sprite = post7S_Imag;
+            post4_Text.text = post7S_Txt;
+
+        }
+        if (post8S_bool)
+        {
+            post4_imag.sprite = post8S_Imag;
+            post4_Text.text = post8S_Txt;
+
+        }
+        if (post9S_bool)
+        {
+            post4_imag.sprite = post9S_Imag;
+            post4_Text.text = post9S_Txt;
+
+        }
+        if (post10S_bool)
+        {
+            post4_imag.sprite = post10S_Imag;
+            post4_Text.text = post10S_Txt;
+
+        }
+    }
 }
